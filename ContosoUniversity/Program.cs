01beef9b13446d5f252a7006be61e09167b64eb8
Tammi Nyberg <tamminyberg@gmail.com>
2022-10-28 12:04:05 +0200

    added classes, views, controllers, migrations etc.

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -17,21 +17,7 @@ namespace ContosoUniversity
         {
             var host = CreateHostBuilder(args).Build();
 
-            //CreateDbIfNotExists(host);
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<SchoolContext>();
-                    DbInitializer.Initialize(context);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while seeding the database.");
-                }
-            }
+            CreateDbIfNotExists(host);
 
             host.Run();
         }
